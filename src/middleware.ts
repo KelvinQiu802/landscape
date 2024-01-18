fd1f5aade9b8fcc23f3be90549923fcf7b835232
KelvinQiu <kelvinqiu802@outlook.com>
2024-01-18 20:25:50 +0800

    feat: update matcher rule

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,6 +10,8 @@ export default createMiddleware({
 });
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', `/(en|zh)/:path*`],
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
 };
