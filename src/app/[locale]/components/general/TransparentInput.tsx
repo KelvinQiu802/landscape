9b80f8c2accd590450d629aad3aac2a49b876cfa
KelvinQiu <kelvinqiu802@outlook.com>
2024-01-19 13:48:53 +0800

    feat: transparent input component

---
+++ src/app/[locale]/components/general/TransparentInput.tsx
@@ -0,0 +1,20 @@
+import React, { InputHTMLAttributes } from 'react';
+import style from './TransparentInput.module.css';
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  fullWidth?: boolean;
+}
+
+function TransparentInput({ fullWidth, ...props }: Props) {
+  return (
+    <input
+      type="text"
+      {...props}
+      className={`${style.input} ${fullWidth ? style.fullWidth : ''} ${
+        props.className
+      }`}
+    />
+  );
+}
+
+export default TransparentInput;
