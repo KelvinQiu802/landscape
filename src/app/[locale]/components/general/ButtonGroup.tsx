6c1e9cc7f7ac0247a4f835e11954d3b57d94beb4
KelvinQiu <kelvinqiu802@outlook.com>
2024-01-19 13:03:42 +0800

    feat: pass props to button group

--- src/app/[locale]/components/general/ButtonGroup.tsx
+++ src/app/[locale]/components/general/ButtonGroup.tsx
@@ -1,13 +1,23 @@
-import { ReactNode } from 'react';
+import { HTMLAttributes, ReactNode } from 'react';
 import style from './ButtonGroup.module.css';
 import Window from './Window';
 
-interface Props {
+interface Props extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
+  hideBackground?: boolean;
 }
 
-function ButtonGroup({ children }: Props) {
-  return <Window className={style.group}>{children}</Window>;
+function ButtonGroup({ children, hideBackground, ...props }: Props) {
+  return (
+    <Window
+      {...props}
+      className={`${style.group} ${hideBackground ? style.hideBg : ''} ${
+        props.className
+      }`}
+    >
+      {children}
+    </Window>
+  );
 }
 
 export default ButtonGroup;
