c01a1556bbb019012c86ea07d8ac9825870e2527
KelvinQiu <kelvinqiu802@outlook.com>
2024-01-19 13:03:56 +0800

    feat: Button component

---
+++ src/app/[locale]/components/general/Button.tsx
@@ -0,0 +1,43 @@
+import { ButtonHTMLAttributes, FC } from 'react';
+import style from './Button.module.css';
+import { SvgIconProps } from '@mui/material/SvgIcon';
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  label?: string;
+  icon?: FC<SvgIconProps>;
+}
+
+function Button(props: Props) {
+  if (props.label && props.icon) {
+    return (
+      /* button with label and icon */
+      <button {...props} className={style.btn}>
+        <props.icon className={style.icon} />
+        {props.label}
+      </button>
+    );
+  } else if (props.label) {
+    return (
+      /* button with only label */
+      <button {...props} className={style.btn}>
+        {props.label}
+      </button>
+    );
+  } else if (props.icon) {
+    return (
+      /* button with only icon */
+      <button {...props} className={`${style.btn} ${style.round}`}>
+        <props.icon className={style.icon} />
+      </button>
+    );
+  } else {
+    return (
+      /* invalid button */
+      <button {...props} className={style.btn}>
+        Must have an icon or label
+      </button>
+    );
+  }
+}
+
+export default Button;
