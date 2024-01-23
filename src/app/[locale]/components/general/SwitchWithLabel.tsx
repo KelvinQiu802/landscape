074f824a0337a4e0e7322a4c891145fa5b08cea6
KelvinQiu <kelvinqiu802@outlook.com>
2024-01-23 20:11:08 +0800

    fix: forward ref

--- src/app/[locale]/components/general/SwitchWithLabel.tsx
+++ src/app/[locale]/components/general/SwitchWithLabel.tsx
@@ -1,19 +1,23 @@
 import { Switch, SwitchProps } from '@mui/material';
+import { forwardRef } from 'react';
 import style from './SwithWithLabel.module.css';
 
 export interface SwitchWithLabelProps extends SwitchProps {
   label: string;
 }
 
-function SwitchWithLabel(props: SwitchWithLabelProps) {
+const SwitchWithLabel = forwardRef(function SwitchWithLabel(
+  props: SwitchWithLabelProps,
+  ref
+) {
   return (
     <div>
       <label className={style.box}>
-        <Switch {...props} />
+        <Switch {...props} ref={ref as any} />
         <div className={style.label}>{props.label}</div>
       </label>
     </div>
   );
-}
+});
 
 export default SwitchWithLabel;
