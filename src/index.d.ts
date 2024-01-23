ce706224ea81d741ea9685f6f79f5288eaee08f0
KelvinQiu <kelvinqiu802@outlook.com>
2024-01-23 14:08:30 +0800

    feat: type of settings

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,3 +4,25 @@ export interface Video {
   title: string;
   coverImg: string;
 }
+
+export enum PlayOrder {
+  random = 0,
+  loop = 1,
+  sequential = 2,
+}
+
+export interface AppSettings {
+  timer: {
+    pomodoro: number;
+    shortBreak: number;
+    longBreak: number;
+  };
+  background: {
+    autoPlay: boolean;
+    playOrder: PlayOrder;
+  };
+  alarm: {
+    type: string;
+    volume: number;
+  };
+}
