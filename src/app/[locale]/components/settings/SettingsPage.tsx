d76d530b7af8d06d6623ebebd93927817b9b48c3
KelvinQiu <kelvinqiu802@outlook.com>
2024-01-23 17:25:00 +0800

    feat: auto play switch

--- src/app/[locale]/components/settings/SettingsPage.tsx
+++ src/app/[locale]/components/settings/SettingsPage.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { AppSettingsContext } from '../MainApp';
 import InputWithLabel from '../general/InputWithLabel';
+import SwitchWithLabel from '../general/SwitchWithLabel';
 import style from './SettingsPage.module.css';
 
 function SettingsPage() {
@@ -14,18 +15,21 @@ function SettingsPage() {
             <InputWithLabel
               label="Pomodoro"
               width={180}
+              name="pomodoro"
               value={settings.timer.pomodoro}
               endAdornment={<div className={style.unit}>mins</div>}
             />
             <InputWithLabel
               label="Short Break"
               width={180}
+              name="shortBreak"
               value={settings.timer.shortBreak}
               endAdornment={<div className={style.unit}>mins</div>}
             />
             <InputWithLabel
               label="Long Break"
               width={180}
+              name="longBreak"
               value={settings.timer.longBreak}
               endAdornment={<div className={style.unit}>mins</div>}
             />
@@ -35,6 +39,15 @@ function SettingsPage() {
       </div>
       <div className={style.tab}>
         <div className={style.title}>Background Video</div>
+        <div className={style.tab}>
+          <SwitchWithLabel
+            label="Auto Play"
+            value={settings.background.autoPlay}
+          />
+          <div className={style.desc}>
+            Auto play will mute the video by default.
+          </div>
+        </div>
         <div className={style.last}></div>
       </div>
       <div className={style.tab}>
