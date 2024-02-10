4291dfe700eda58e1e37bbe6fe79c5f64f52432c
KelvinQiu <kelvinqiu802@outlook.com>
2024-02-10 13:38:23 +0800

    feat: add repo link

--- src/app/[locale]/components/general/MediaButtons.tsx
+++ src/app/[locale]/components/general/MediaButtons.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import Link from 'next/link';
 import Button from './Button';
 import ButtonGroup, { ButtonGroupProps } from './ButtonGroup';
 
@@ -8,8 +11,12 @@ interface MediaButtonProps extends ButtonGroupProps {}
 function MediaButtons(props: MediaButtonProps) {
   return (
     <ButtonGroup {...props}>
-      <Button icon={GitHubIcon} />
-      <Button icon={FavoriteIcon} />
+      <Link href="https://github.com/kelvinqiu802/landscape" target="_blank">
+        <Button icon={GitHubIcon} />
+      </Link>
+      <Link href="https://github.com/kelvinqiu802/landscape" target="_blank">
+        <Button icon={FavoriteIcon} />
+      </Link>
     </ButtonGroup>
   );
 }
